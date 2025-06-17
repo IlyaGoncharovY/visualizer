3080f7d50e51d2e59c490297cddb117f9dc39e9e
IlyaGoncharovY <goncharov.i@rambler.ru>
2025-06-17 22:02:55 +0300

    feat(ci) added jobs for ci gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/visualizer',
   plugins: [react()],
   resolve: {
     alias: {
@@ -12,6 +13,11 @@ export default defineConfig({
     },
   },
   server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
     port: 3000,
   },
 });
